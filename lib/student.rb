7341a4740c7ab478f6a07d59707b3d2ffad8d58c
Robert McCary <robert.mccary@yahoo.com>
2019-02-15 23:49:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,4 +6,15 @@ attr_accessor :name, :grade
     @name=name
     @grade=grade
   end
+
+  def self.create_table 
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      ) 
+      SQL 
+      DB[:conn].execute(sql)
+  end
 end
